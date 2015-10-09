b81b5dbab89bb817b076354b11add9b25c48c3b2
Zobair Alijan <zobair@Zobairs-MacBook-Air.local>
2015-10-09 15:55:29 -0400

    update

--- universities/spiders/ND_EDU/Notre_Dam.py
+++ universities/spiders/ND_EDU/Notre_Dam.py
@@ -51,27 +51,27 @@ class NotreDameUniversitySpider(scrapy.Spider):
             if link:
                 yield Request('http://architecture.nd.edu%s' %link[0], callback=self.parse_arc_profile)
             else:
-                bii = University()
-                bii['name'] = p.xpath('descendant-or-self::h3/text()').extract()[0]
-                bii['title'] = p.xpath('descendant-or-self::p[2]/text()').extract()[0]
-                bii['institution'] = 'School of Architecture'
-                yield bii
+                item = University()
+                item['name'] = p.xpath('descendant-or-self::h3/text()').extract()[0]
+                item['title'] = p.xpath('descendant-or-self::p[2]/text()').extract()[0]
+                item['institution'] = 'School of Architecture'
+                yield item
 
     def parse_arc_profile(self, response):
         sel = Selector(response)
 
-        bii = University()
-        bii['name'] = sel.xpath('//h1/text()').extract()[0]
-        bii['title'] = sel.xpath('//h2/text()').extract()[0]
-        bii['institution'] = 'School of Architecture'
+        item = University()
+        item['name'] = sel.xpath('//h1/text()').extract()[0]
+        item['title'] = sel.xpath('//h2/text()').extract()[0]
+        item['institution'] = 'School of Architecture'
         phone = sel.xpath('//b[contains(text(), "Phone:")]/following-sibling::text()').extract()
         if phone:
-            bii['phone'] = phone[0].strip()
+            item['phone'] = phone[0].strip()
 
         email = sel.xpath('//b[contains(text(), "Email:")]/following-sibling::a/text()').extract()
         if email:
-            bii['email'] = email[0].strip()
-        yield bii
+            item['email'] = email[0].strip()
+        yield item
 
     def parse_arts_and_letters(self, response):
         """
@@ -82,30 +82,30 @@ class NotreDameUniversitySpider(scrapy.Spider):
         people_sel = sel.xpath('//ul[@id="people-list"]/li')
 
         for profile_sel in people_sel:
-            bii = University()
+            item = University()
 
             name = profile_sel.xpath('div/div/b[@class="person-fullname"]/a/text()').extract()
             if name:
-                bii['name'] = name[0].strip()
+                item['name'] = name[0].strip()
 
             title = profile_sel.xpath('div/div/span[@class="person-title"]/text()').extract()
             if title:
-                bii['title'] = title[0].strip()
+                item['title'] = title[0].strip()
 
             department = profile_sel.xpath('div/div/span[@class="person-department"]/a/text()').extract()
             if department:
-                bii['department'] = department[0]
+                item['department'] = department[0]
 
-            bii['institution'] = 'Mendoza College of Business'
+            item['institution'] = 'Mendoza College of Business'
 
             email = profile_sel.xpath('div/div/div[@class="person-email"]/a/text()').extract()
             if email:
-                bii['email'] = email[0].strip()
+                item['email'] = email[0].strip()
 
             phone = profile_sel.xpath('div/div/div[@class="person-phone"]/text()').extract()
             if phone:
-                bii['phone'] = phone[0].strip()
+                item['phone'] = phone[0].strip()
 
-            yield bii
+            yield item
 
 
